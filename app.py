54957288bf75afab8f71da8e0391e2e18e2e07e4
faical-allou <faical.allou@gmail.com>
2019-07-07 12:50:38 +0200

    first commit with a flask app

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask, jsonify, render_template, request, send_from_directory
+import psycopg2
+import os
+import json
+import collections
+import datetime
+import sys
+import math
+import gc
+
+app = Flask(__name__)
+
+@app.route('/')
+def render_home():
+    return 'Hello, Flocals!'
+
+@app.route('/<path:filename>', methods=['GET'])
+def display_static():
+    return send_from_directory(app.static_folder, filename)
+
+@app.route('/js/<path:filename>', methods=['GET'])
+def load_js(filename):
+    return send_from_directory('js', filename)
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+@app.errorhandler(404)
+def pageNotFound(error):
+    return "sorry problem"
+
+@app.errorhandler(500)
+def erroronpage(error):
+    return "sorry problem"
+
+
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 80.
+    port = int(os.environ.get('PORT', 5000))
+    if os.environ.get('ON_HEROKU'):
+        app.run(host='0.0.0.0', port=port)
+    else :
+        app.run(host='localhost', port=port)
