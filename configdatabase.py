abea4794df22241d06d6cb50e222079728348971
Faical Allou <faical.allou@gmail.com>
2019-07-09 22:12:27 +0200

    Create configdatabase.py

---
+++ configdatabase.py
@@ -0,0 +1,10 @@
+##add your database config here for local developement
+## these are overwritten by environment variables
+
+connectionStringDatabase = "host='' port='' dbname='' user='' password=''"
+
+CLIENT_ID = ''
+CLIENT_SECRET = ''
+AUTH_REDIRECT_URI = ''
+BASE_URI = ''
+FLASK_SECRET_KEY = ''
