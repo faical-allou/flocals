f5a23b051c5745fa737c2bf958ab89c91ba17d05
faical-allou <faical.allou@gmail.com>
2019-07-26 23:21:41 +0200

    return latlong in item list

--- models/extractdata.py
+++ models/extractdata.py
@@ -78,10 +78,10 @@ class extractdata:
         connection = self.getconnection()
         cursor = connection.cursor(cursor_factory=RealDictCursor)
         query = "WITH cte_location as (SELECT latitude, longitude FROM airports where lower(iata) = lower(%s))\
-                SELECT place_id, rec_name, count(1) as nb_rec \
+                SELECT place_id, rec_name, lat, lng, count(1) as nb_rec \
                 FROM recommendations as r CROSS JOIN cte_location as l\
                 WHERE lower(r.type_convert) = lower(%s) AND gcd(r.lat, r.lng, l.latitude, l.longitude)<100 \
-                GROUP BY place_id,rec_name order by count(1) DESC"
+                GROUP BY place_id,rec_name, lat, lng order by count(1) DESC"
         cursor.execute(query,(airport_, type_,))
         result = json.dumps(cursor.fetchall(), indent=2)
         connection.close()
